0f000dd889d82b50173a1919dd9d46d40b27cace
tanuj gyan <tanujgyan@geotab.com>
2025-03-23 20:38:41 -0400

    initial commit

---
+++ app/api/DTO/basemodels.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+from app.api.DTO.marketenums import MarketEnum
+
+
+class StockData(BaseModel):
+    ticker: str
+    name: str
+    market: MarketEnum
+    locale: str
+    primary_exchange: str
+    type: str
+    active: bool
+    currency_name: str
+    cik: Optional[str]
+    composite_figi: Optional[str]
+    share_class_figi: Optional[str]
+    #last_updated_utc: datetime
+
+class HistoricalData(BaseModel):
+    ticker: str
+    queryCount: int
+    resultsCount: int
+    adjusted: bool
+    results: List[dict]
+    status: str
+    request_id: str
+    count: int
+
+class StockSearch(BaseModel):
+    results: List[StockData]
+    count: int
+    status: str
+    request_id: str
--- app/api/endpoints/stocks.py
+++ app/api/endpoints/stocks.py
@@ -1,7 +1,44 @@
-from fastapi import APIRouter
-
+import os
+import requests
+from dotenv import load_dotenv
+from fastapi import APIRouter, \
+    HTTPException
+from ..DTO.basemodels import StockData, HistoricalData, StockSearch
+load_dotenv()
 router = APIRouter()
+POLYGON_API_KEY = os.getenv("FINANCIAL_API_KEY")
+BASE_URL = "https://api.polygon.io"
 
 @router.get("/stocks")
 async def read_items():
     return {"message": "Endpoint works!"}
+
+@router.get("/stocks/{symbol}", response_model=StockData)
+async def get_stock_data(symbol: str):
+    url = f"{BASE_URL}/v3/reference/tickers/{symbol}?apiKey={POLYGON_API_KEY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()["results"]
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch stock data")
+
+@router.get("/stocks/{symbol}/historical")
+async def get_historical_data(symbol: str, date_from: str, date_to: str, multiplier: int, timespan: str):
+    url = f"{BASE_URL}/v2/aggs/ticker/{symbol}/range/{multiplier}/{timespan}/{date_from}/{date_to}?adjusted=true&apiKey={POLYGON_API_KEY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch historical data")
+
+@router.get("/stocks/search/{query}")
+async def search_stocks(query: str):
+    url = f"{BASE_URL}/v3/reference/tickers?search={query}&apiKey={POLYGON_API_KEY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch search results")
